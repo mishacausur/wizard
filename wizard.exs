ae100e3cfdceb00d69f26fd81bf31b727905577a
Misha Causur <mishacausur@mail.ru>
2024-07-28 01:08:13 +0500

    calzulation

--- wizard.exs
+++ wizard.exs
@@ -4,5 +4,11 @@ defmodule Wizard do
 	end
 
 	def calculate(number) do
+		cond do
+			rem(number, 3) == 0 and rem(number, 5) == 0 -> IO.puts("both")
+			rem(number, 3) == 0 -> IO.puts("three")
+			rem(number, 5) == 0 -> IO.puts("five")
+			true -> IO.puts(number)
+		end
 	end
 end
