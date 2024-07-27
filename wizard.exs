d112cdee6199e5e66686411eced179eb62700eb8
Misha Causur <mishacausur@mail.ru>
2024-07-28 01:14:06 +0500

    retuning values

--- wizard.exs
+++ wizard.exs
@@ -4,12 +4,13 @@ defmodule Wizard do
 		Enum.each(1..100, &calculate/1)
 	end
 
+	@spec calculate(integer()) :: String.t()
 	def calculate(number) do
 		cond do
-			rem(number, 3) == 0 and rem(number, 5) == 0 -> IO.puts("both")
-			rem(number, 3) == 0 -> IO.puts("three")
-			rem(number, 5) == 0 -> IO.puts("five")
-			true -> IO.puts(number)
+			rem(number, 3) == 0 and rem(number, 5) == 0 -> "both"
+			rem(number, 3) == 0 -> "three"
+			rem(number, 5) == 0 -> "five"
+			true -> to_string(number)
 		end
 	end
 end
