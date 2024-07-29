c50267066d564556846aba56b0856ee812e9635a
Misha Causur <mishacausur@mail.ru>
2024-07-29 23:57:29 +0500

    unit tests added

--- wizard.exs
+++ wizard.exs
@@ -1,10 +1,14 @@
 defmodule Wizard do
 
 	def main() do
-		1..100
-		|> Enum.map(&calculate/1)
+		calculate100()
 		|> Enum.join(" ")
 		|> IO.puts()
+	end
+
+	def calculate100() do
+
+		Enum.map(1..100, &calculate/1)
 	end
 
 	@spec calculate(integer()) :: String.t()
@@ -17,5 +21,25 @@ defmodule Wizard do
 			divisible_by_5 -> "five"
 			true -> to_string(number)
 		end
+	end
+end
+
+ExUnit.start()
+
+defmodule WizardTest do
+	use ExUnit.Case
+	import Wizard
+
+	test "calculate 1-10" do
+		assert calculate(1) == "1"
+		assert calculate(2) == "2"
+		assert calculate(3) == "three"
+		assert calculate(4) == "4"
+		assert calculate(5) == "five"
+		assert calculate(6) == three"
+		assert calculate(7) == "7"
+		assert calculate(8) == "8"
+		assert calculate(9) == three"
+		assert calculate(10) == "five"
 	end
 end
