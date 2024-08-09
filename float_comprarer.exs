5e1e5ffe3a5c94d525aaf241acf75081e5e35596
Misha Causur <mishacausur@mail.ru>
2024-08-09 22:47:15 +0500

    tests for float w/ refute

--- float_comprarer.exs
+++ float_comprarer.exs
@@ -4,3 +4,21 @@ defmodule FloatComprarer do
     abs(f1 - f2) < presicion
   end
 end
+
+ExUnit.start()
+defmodule FloatComprarerTests do
+  use ExUnit.Case
+  import FloatComprarer
+
+  test "equal?" do
+    assert equal?(5.0, 5.0)
+    assert not equal?(11.3, 11.5)
+    refute equal?(3.4, 9.8)
+    assert equal?(15.06, 15.06)
+  end
+
+  test "equal? w/ prec" do
+    assert equal?(5.3, 5.31)
+    refute equal?(3.14, 3.1, 0.001)
+  end
+end
